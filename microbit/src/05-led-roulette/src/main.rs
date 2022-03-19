9f8e6ce352f131037d6bad9e3ad3db36a705f788
Carlos A. Parra F <carlos.a.parra.f@gmail.com>
2022-03-19 14:54:39 +0100

    Working on rouylette

--- microbit/src/05-led-roulette/src/main.rs
+++ microbit/src/05-led-roulette/src/main.rs
@@ -3,15 +3,61 @@
 #![no_std]
 
 use cortex_m_rt::entry;
-use panic_halt as _;
-use microbit as _;
+use microbit::{
+    board::Board,
+    display::blocking::Display,
+    hal::{prelude::*, Timer},
+};
+use panic_rtt_target as _;
+use rtt_target::{rprintln, rtt_init_print};
+
+fn get_next_led(x0: u8, y0: u8) -> [[u8; 5]; 5] {
+    let mut led_matrix = [
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+    ];
+    let x1, y1 = if y0 == 0 {
+        if x0 == 4 {
+            4
+        } else {
+            x0 + 1
+        }
+    } else if y0 == 4 {
+        if x0 == 0 {
+            0
+        } else {
+            x0 - 1
+        }
+    } else {
+        x0
+    };
+}
 
 #[entry]
 fn main() -> ! {
-    let _y;
-    let x = 42;
-    _y = x;
+    // Necessary to get cmd output
+    rtt_init_print!();
 
-    // infinite loop; just so we don't leave this stack frame
-    loop {}
+    let mut board = Board::take().unwrap();
+    let mut timer = Timer::new(board.TIMER0);
+    let mut display = Display::new(board.display_pins);
+
+    // let mut row1 = board.display_pins.row1;
+    let led_matrix = [
+        [1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1],
+    ];
+    loop {
+        // timer.delay_ms(1000u16);
+        // rprintln!("1000 ms passed");
+        display.show(&mut timer, led_matrix, 1000);
+        display.clear();
+        timer.delay_ms(1000_u32);
+    }
 }
