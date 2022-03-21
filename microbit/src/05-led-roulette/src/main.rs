4af39df4b67602d955036a0aa2e424d1920d0726
tuto193 <carlos.a.parra.f@gmail.com>
2022-03-21 22:14:06 +0100

    Finished my LED

--- microbit/src/05-led-roulette/src/main.rs
+++ microbit/src/05-led-roulette/src/main.rs
@@ -9,24 +9,19 @@ use microbit::{
     hal::{prelude::*, Timer},
 };
 use panic_rtt_target as _;
-use rtt_target::{rprintln, rtt_init_print};
+use rtt_target::{
+    // rprintln,
+    rtt_init_print
+};
 
-fn get_next_led(x0: u8, y0: u8) -> [[u8; 5]; 5] {
-    let mut led_matrix: [[u8; 5]; 5] = [
-        [0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0],
-    ];
-    let (x1, y1) = match (x0, y0) {
-        (4, y) if y < 4 => (4, y0 + 1),
-        (x, 0) if x < 4 => (x0 + 1, 0),
-        (0, y) if y > 0 => (0, y0 - 1),
-        (x, 4) if x > 0 => (x0 - 1, 4),
-    };
-    led_matrix[x1][y1] = 1;
-    led_matrix
+fn get_next_led(coords: (usize, usize)) -> (usize, usize) {
+    match coords {
+        (4, y) if y < 4 => (4, y + 1),
+        (x, 0) if x < 4 => (x + 1, 0),
+        (0, y) if y > 0 => (0, y - 1),
+        (x, 4) if x > 0 => (x - 1, 4),
+        (_, _) => (0, 0),
+    }
 }
 
 #[entry]
@@ -39,18 +34,19 @@ fn main() -> ! {
     let mut display = Display::new(board.display_pins);
 
     // let mut row1 = board.display_pins.row1;
-    let led_matrix = [
-        [1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1],
+    let mut led_matrix = [
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
     ];
+    let mut coord : (usize, usize) = (0, 0);
     loop {
-        // timer.delay_ms(1000u16);
         // rprintln!("1000 ms passed");
-        display.show(&mut timer, led_matrix, 1000);
-        display.clear();
-        timer.delay_ms(1000_u32);
+        led_matrix[coord.0][coord.1] = 0;
+        coord = get_next_led(coord);
+        led_matrix[coord.0][coord.1] = 1;
+        display.show(&mut timer, led_matrix, 50);
     }
 }
