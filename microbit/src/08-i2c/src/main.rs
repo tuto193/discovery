a2475f5561aa48cfeaa1d5bb9da3a7fd756c4d5f
tuto193 <carlos.a.parra.f@gmail.com>
2022-04-10 19:04:54 +0200

    Started challenge

--- microbit/src/08-i2c/src/main.rs
+++ microbit/src/08-i2c/src/main.rs
@@ -2,35 +2,49 @@
 #![no_main]
 #![no_std]
 
+use core::fmt::Write;
 use cortex_m_rt::entry;
-use rtt_target::{rtt_init_print, rprintln};
+use heapless::Vec;
 use panic_rtt_target as _;
+use panic_rtt_target as _;
+use rtt_target::{rprintln, rtt_init_print};
 
 use microbit::hal::prelude::*;
 
 #[cfg(feature = "v1")]
-use microbit::{
-    hal::twi,
-    pac::twi0::frequency::FREQUENCY_A,
-};
+use microbit::{hal::twi, pac::twi0::frequency::FREQUENCY_A};
 
 #[cfg(feature = "v2")]
 use microbit::{
+    hal::prelude::*,
     hal::twim,
+    hal::uarte,
+    hal::uarte::{Baudrate, Parity},
     pac::twim0::frequency::FREQUENCY_A,
 };
 
-const ACCELEROMETER_ADDR: u8 = 0b0011001;
-const MAGNETOMETER_ADDR: u8 = 0b0011110;
+#[cfg(feature = "v2")]
+mod serial_setup;
+#[cfg(feature = "v2")]
+use serial_setup::UartePort;
 
-const ACCELEROMETER_ID_REG: u8 = 0x0f;
-const MAGNETOMETER_ID_REG: u8 = 0x4f;
+use lsm303agr::{AccelOutputDataRate, Lsm303agr};
 
 #[entry]
 fn main() -> ! {
     rtt_init_print!();
     let board = microbit::Board::take().unwrap();
 
+    #[cfg(feature = "v2")]
+    let mut serial = {
+        let serial = uarte::Uarte::new(
+            board.UARTE0,
+            board.uart.into(),
+            Parity::EXCLUDED,
+            Baudrate::BAUD115200,
+        );
+        UartePort::new(serial)
+    };
 
     #[cfg(feature = "v1")]
     let mut i2c = { twi::Twi::new(board.TWI0, board.i2c.into(), FREQUENCY_A::K100) };
@@ -38,15 +52,20 @@ fn main() -> ! {
     #[cfg(feature = "v2")]
     let mut i2c = { twim::Twim::new(board.TWIM0, board.i2c_internal.into(), FREQUENCY_A::K100) };
 
-    let mut acc = [0];
-    let mut mag = [0];
+    // Code from docs
+    let mut sensor = Lsm303agr::new_with_i2c(i2c);
+    sensor.init().unwrap();
+    sensor.set_accel_odr(AccelOutputDataRate::Hz50).unwrap();
 
-    // First write the address + register onto the bus, then read the chip's responses
-    i2c.write_read(ACCELEROMETER_ADDR, &[ACCELEROMETER_ID_REG], &mut acc).unwrap();
-    i2c.write_read(MAGNETOMETER_ADDR, &[MAGNETOMETER_ID_REG], &mut mag).unwrap();
-
-    rprintln!("The accelerometer chip's id is: {:#b}", acc[0]);
-    rprintln!("The magnetometer chip's id is: {:#b}", mag[0]);
-
-    loop {}
+    loop {
+        if sensor.accel_status().unwrap().xyz_new_data {
+            let data = sensor.accel_data().unwrap();
+            rprintln!(
+                "Acceleration:\n\tx\t{}\n\ty\t{}\n\tz\t{}",
+                data.x,
+                data.y,
+                data.z
+            )
+        }
+    }
 }
